3f4635ca6dcfeeb1b69994943f963e064c330c96
Andrew <atklenner@gmail.com>
2022-01-23 12:12:50 -0800

    Finish comment form

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,11 +1,47 @@
+import { useState } from "react";
+import { DATABASE_URL } from "../urls";
+
 export default function CommentForm(props) {
-  // you have to do a lot more for forms in React
+  const [author, setAuthor] = useState("");
+  const [body, setBody] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const url = `${DATABASE_URL}/blog/${props.blogID}/comments`;
+    const comment = {
+      body,
+      author,
+      blogPost: props.blogID,
+    };
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(comment),
+    };
+    fetch(url, requestOptions)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <form action={`/blog/${props.blogID}/comments`} method="POST">
+    <form onSubmit={handleSubmit}>
       <label>Name: </label>
-      <input type="text" placeholder="Name" name="author" id="author" />
+      <input
+        type="text"
+        placeholder="Name"
+        name="author"
+        id="author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
       <label>Comment: </label>
-      <textarea placeholder="Comment away" name="body" id="body"></textarea>
+      <textarea
+        placeholder="Comment away"
+        name="body"
+        id="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
       <button>Submit</button>
     </form>
   );
