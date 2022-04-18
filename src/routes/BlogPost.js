881b58d3663e6b15e960a98921004edf531f645e
Andrew <atklenner@gmail.com>
2022-04-18 12:30:04 -0700

    Add markdown parsing

--- src/routes/BlogPost.js
+++ src/routes/BlogPost.js
@@ -6,6 +6,7 @@ import Comments from "../components/Comments";
 import Header from "../components/Header";
 import styles from "../styles/BlogPost.module.css";
 import Footer from "../components/Footer";
+import ReactMarkdown from "react-markdown";
 
 export default function BlogPost() {
   const [blogPost, setBlogPost] = useState([]);
@@ -47,7 +48,8 @@ export default function BlogPost() {
           <p className={styles.post__date}>
             {new Date(blogPost.date).toLocaleString()}
           </p>
-          <p className={styles.post__body}>{blogPost.body}</p>
+          {/* <p className={styles.post__body}>{blogPost.body}</p> */}
+          <ReactMarkdown>{blogPost.body}</ReactMarkdown>
           <p className={styles.post__tags}>{blogPost.tags}</p>
         </article>
         <hr></hr>
