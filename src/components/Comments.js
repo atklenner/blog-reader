a817e6bed93e5ed93039605be3d3f2d5a3a3a4e4
Andrew <atklenner@gmail.com>
2022-04-17 23:36:07 -0700

    Cleanup

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,21 +1,19 @@
 import styles from "../styles/Comments.module.css";
 
-export default function Comments(props) {
-  function displayComments(commentsArray) {
-    if (commentsArray) {
-      return commentsArray.map((comment) => {
+export default function Comments({ comments }) {
+  return (
+    <div>
+      {comments.map((comment) => {
         return (
           <div key={comment._id} className={styles.comment__container}>
             <p className={styles.comment__body}>{comment.body}</p>
             <p className={styles.comment__author}>{comment.author}</p>
             <p className={styles.comment__date}>
-              {/* {comment.date} */}
               {new Date(comment.date).toLocaleString()}
             </p>
           </div>
         );
-      });
-    } else return "";
-  }
-  return <div>{displayComments(props.comments)}</div>;
+      })}
+    </div>
+  );
 }
